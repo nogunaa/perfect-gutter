78792bc010edadfaf1fecef55ee39268db7b3ee9
Nogunaa <nogunaa@gmail.com>
2023-03-30 16:11:09 -0600

    review slide bug fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let navbar = document.querySelector('.header .navbar');
-let searchForm = document.querySelector('.header .search-form');
-let loginForm = document.querySelector('.header .login-form');
+// let searchForm = document.querySelector('.header .search-form');
+// let loginForm = document.querySelector('.header .login-form');
 let contactInfo = document.querySelector('.contact-info');
 
 document.querySelector('#menu-btn').onclick = () =>{
@@ -9,17 +9,17 @@ document.querySelector('#menu-btn').onclick = () =>{
    loginForm.classList.remove('active');
 };
 
-document.querySelector('#search-btn').onclick = () =>{
-   searchForm.classList.toggle('active');
-   navbar.classList.remove('active');
-   loginForm.classList.remove('active');
-};
+// document.querySelector('#search-btn').onclick = () =>{
+//    searchForm.classList.toggle('active');
+//    navbar.classList.remove('active');
+//    loginForm.classList.remove('active');
+// };
 
-document.querySelector('#login-btn').onclick = () =>{
-   loginForm.classList.toggle('active');
-   navbar.classList.remove('active');
-   searchForm.classList.remove('active'); 
-};
+// document.querySelector('#login-btn').onclick = () =>{
+//    loginForm.classList.toggle('active');
+//    navbar.classList.remove('active');
+//    searchForm.classList.remove('active'); 
+// };
 
 document.querySelector('#info-btn').onclick = () =>{
    contactInfo.classList.add('active');
@@ -31,8 +31,8 @@ document.querySelector('#close-contact-info').onclick = () =>{
 
 window.onscroll = () =>{
    navbar.classList.remove('active');
-   searchForm.classList.remove('active');
-   loginForm.classList.remove('active');
+   // searchForm.classList.remove('active');
+   // loginForm.classList.remove('active');
    contactInfo.classList.remove('active');
 }
 
